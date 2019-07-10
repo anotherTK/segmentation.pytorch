dc17bc3f04dbdaad45fb2925451307c5777c3f01
tkianai <tkianai@163.com>
2019-07-10 13:10:31 +0800

    add utils for submit

--- scripts/prepare_pretrained_model.py
+++ scripts/prepare_pretrained_model.py
@@ -5,8 +5,9 @@ import torch
 
 def trim(model_state, keys):
     for key in keys:
-        if key in model_state.keys():
-            model_state.pop(key)
+        for true_key in model_state.keys():
+            if key in true_key:
+                model_state.pop(true_key)
 
     return model_state
 
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     
     model = torch.load('work_dirs/pretrained/encnet_jpu_resnet101_pcontext.pth', map_location='cpu')
 
-    model_state = trim(model['model'], ['head.encmodule.selayer', 'head.conv6', 'auxlayer']
+    model_state = trim(model['model'], ['head.encmodule.selayer', 'head.conv6', 'auxlayer'])
     
     model['model'] = model_state
     torch.save(model, 'work_dirs/pretrained/encnet_jpu_resnet101_pcontext_trimed.pth')
